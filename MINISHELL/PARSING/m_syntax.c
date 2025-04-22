f60aa556854cbe3ca4cf1525b2781d333616b6ce
Cedric Bauer <cbauer@3-C-1.42heilbronn.de>
2025-04-22 17:16:24 +0200

    fixing syntax problems

--- MINISHELL/PARSING/m_syntax.c
+++ MINISHELL/PARSING/m_syntax.c
@@ -6,7 +6,7 @@
 /*   By: cbauer < cbauer@student.42heilbronn.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/14 14:01:32 by cbauer            #+#    #+#             */
-/*   Updated: 2025/04/20 13:11:43 by cbauer           ###   ########.fr       */
+/*   Updated: 2025/04/22 17:15:15 by cbauer           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,13 +23,13 @@ int	syntax_redirects(t_token **cur)
 	}
 	else if ((*cur)->type == TOKEN_HEREDOC)
 	{
-		if ((*cur)->next == NULL && ((*cur)->value == NULL
-				|| (*cur)->value[0] == '\0'))
-			return (choose_error_statement(1, NULL), -1);
-		else if ((*cur)->next != NULL && (*cur)->next->type == TOKEN_HEREDOC)
+		// if ((*cur)->next == NULL && ((*cur)->value == NULL
+		// 		|| (*cur)->value[0] == '\0'))
+		// 	return (choose_error_statement(1, NULL), -1);
+		if ((*cur)->next != NULL && (*cur)->next->type == TOKEN_HEREDOC)
 			return (choose_error_statement(2, "<<"), -1);
 		else if ((*cur)->value == NULL || (*cur)->value[0] == '\0')
-			return (choose_error_statement(2, (*cur)->next->value), -1);
+			return (write(1, "here\n", 5), choose_error_statement(2, (*cur)->next->value), -1);
 	}
 	return (0);
 }
