a937d7f6c075223e77d8366f1dfc8cceee587df3
Cedric Bauer <cbauer@3-C-6.42heilbronn.de>
2025-05-05 14:49:28 +0200

    just norms

--- EXECUTION/builtin_utils.c
+++ EXECUTION/builtin_utils.c
@@ -3,49 +3,48 @@
 /*                                                        :::      ::::::::   */
 /*   builtin_utils.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jisokim2 <jisokim2@student.42heilbronn.    +#+  +:+       +#+        */
+/*   By: cbauer < cbauer@student.42heilbronn.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/05 14:16:18 by jisokim2          #+#    #+#             */
-/*   Updated: 2025/05/05 14:16:19 by jisokim2         ###   ########.fr       */
+/*   Updated: 2025/05/05 14:23:21 by cbauer           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "execution.h"
 
-char	*find_var_in_env(char **my_envp, char *find, size_t find_len, t_gc_list *gc_lst)
+char	*find_var_in_env(char **my_envp, char *find, size_t find_len, \
+	t_gc_list *gc_lst)
 {
 	int		i;
 	char	*result;
 
 	i = 0;
-	while(my_envp[i])
+	while (my_envp[i])
 	{
-		if (strncmp(my_envp[i], find, find_len) == 0 && my_envp[i][find_len] == '=')
+		if (strncmp(my_envp[i], find, find_len) == 0
+			&& my_envp[i][find_len] == '=')
 		{
 			result = my_envp[i] + find_len + 1;
-			return gc_strdup(result, &gc_lst);
+			return (gc_strdup(result, &gc_lst));
 		}
 		i++;
 	}
-	return NULL;
+	return (NULL);
 }
 
-//for export and unset
 bool	is_valid_identifier(const char *name)
 {
 	int	i;
 
 	i = 0;
-	if(!name || (!ft_isalpha(name[0]) && name[0] != '_'))
-	{
-		return false;
-	}
+	if (!name || (!ft_isalpha(name[0]) && name[0] != '_'))
+		return (false);
 	i = 1;
-	while(name[i])
+	while (name[i])
 	{
-		if(!ft_isalpha(name[i]) && name[i] != '_')
-			return false;
+		if (!ft_isalpha(name[i]) && name[i] != '_')
+			return (false);
 		i++;
 	}
-	return true;
+	return (true);
 }
--- EXECUTION/cd.c
+++ EXECUTION/cd.c
@@ -3,93 +3,74 @@
 /*                                                        :::      ::::::::   */
 /*   cd.c                                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jisokim2 <jisokim2@student.42heilbronn.    +#+  +:+       +#+        */
+/*   By: cbauer < cbauer@student.42heilbronn.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/05 14:16:23 by jisokim2          #+#    #+#             */
-/*   Updated: 2025/05/05 14:16:24 by jisokim2         ###   ########.fr       */
+/*   Updated: 2025/05/05 14:49:07 by cbauer           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-/*
-	cd[path] : move to path;
-	cd or cd ~ : move to home;
-	cd -: move to prev dir;
-	cd ..: move to parent dir;
-	cd /: move to root dir;
-*/
-
 #include "execution.h"
 
-//stat(): check path, and check if its approchable. succesed return 0, failed: return -1
-//S_ISDIR is a macro 
-//S_ISFIFO macro to check is pipe?
-//S_IFREG macro to check is normal file?
-//S_IFLNK	simbolick link
-
-//ok!
 int	is_valid_dir(const char *path)
 {
-	struct stat file_stat;
+	struct stat	file_stat;
 
 	if (stat(path, &file_stat) != 0)
 	{
 		perror("failed stat()");
-		return 0;
+		return (0);
 	}
 	else
 	{
 		if (S_ISDIR(file_stat.st_mode))
-		{
-			//printf(YELLOW "is_valid_dir() found path %s\n" DEFAULT, path);
-			return 1;
-		}
+			return (1);
 	}
 	return (0);
 }
 
-//ok!
 int	check_existing(char **my_envp, const char *name)
 {
 	int	i;
 
 	i = 0;
-	while(my_envp[i])
+	while (my_envp[i])
 	{
-		if ((strncmp(my_envp[i], name, strlen(name)) == 0) && (my_envp[i][strlen(name)] == '='))
-		{
+		if ((strncmp(my_envp[i], name, strlen(name)) == 0)
+			&& (my_envp[i][strlen(name)] == '='))
 			return (i);
-		}
 		i++;
 	}
-	return(-1);
+	return (-1);
 }
 
 int	get_env_count(char **my_envp)
 {
-	int env_count;
+	int	env_count;
 
 	env_count = 0;
-	while(my_envp[env_count])
+	while (my_envp[env_count])
 		env_count++;
-	return env_count;
+	return (env_count);
 }
 
 char	**expand_envp(t_shell *shell, char *new_path)
 {
-	int env_count;
-	int	i;
-	t_gc *gc;
+	int		env_count;
+	int		i;
+	t_gc	*gc;
+	char	**new_envp;
 
 	gc = get_gc();
 	env_count = get_env_count(shell->my_envp);
-	char	**new_envp = malloc(sizeof(char*) * (env_count + 2));
+	new_envp = malloc(sizeof(char *) * (env_count + 2));
 	if (!new_envp)
 	{
 		gc_free(gc);
 		exit(1);
 	}
 	i = 0;
-	while(shell->my_envp[i])
+	while (shell->my_envp[i])
 	{
 		new_envp[i] = shell->my_envp[i];
 		i++;
@@ -99,34 +80,33 @@ char	**expand_envp(t_shell *shell, char *new_path)
 	return (new_envp);
 }
 
-char *create_new_path(const char *name, const char *value)
+char	*create_new_path(const char *name, const char *value)
 {
-	int	namelen;
-	int	valuelen;
-
-	t_gc *gc;
+	int		namelen;
+	int		valuelen;
+	t_gc	*gc;
+	char	*new_path;
 
 	gc = get_gc();
 	if (!value)
 	{
 		namelen = ft_strlen(name);
-		//todo &gc->temp ? or &gc->shell
-		char *new_path = do_alloc(&gc->shell, namelen + 1, TYPE_SINGLE_PTR, "new_path");
+		new_path = do_alloc(&gc->shell, namelen + 1, \
+			TYPE_SINGLE_PTR, "new_path");
 		if (!new_path)
 		{
 			perror("failed to create new_path");
 			return (NULL);
 		}
 		ft_strlcpy(new_path, name, namelen + 1);
-		// if(new_path[namelen] == '\0')
-		// 	printf(RED"null\n"DEFAULT);
-		return new_path;
+		return (new_path);
 	}
 	else
 	{
 		namelen = ft_strlen(name);
 		valuelen = ft_strlen(value);
-		char *new_path = do_alloc(&gc->shell, namelen + valuelen + 2, TYPE_SINGLE_PTR, "new_path");
+		new_path = do_alloc(&gc->shell, namelen + valuelen + 2, \
+			TYPE_SINGLE_PTR, "new_path");
 		if (!new_path)
 		{
 			perror("failed to create new_path");
@@ -139,46 +119,50 @@ char *create_new_path(const char *name, const char *value)
 	}
 }
 
-void	ft_setenv(const char *name, const char *value, int overwrite, t_shell *shell)
+void	ft_setenv(const char *name, const char *value, \
+	int overwrite, t_shell *shell)
 {
-	int		index;
-	char	*new_path;
-	char	**new_envp;
-	t_gc	*gc;
+	int			index;
+	char		*new_path;
+	char		**new_envp;
+	t_gc		*gc;
+	t_gc_list	*find;
+	t_gc_list	*old_envp_node;
+	char		**old_env;
 
-	if(!name || *name == '\0')
+	if (!name || *name == '\0')
 		return ;
 	gc = get_gc();
 	index = check_existing(shell->my_envp, name);
 	new_path = create_new_path(name, value);
 	is_exited(new_path, gc);
-	// already exists but overwrite is false → do nothing
-	if(index >= 0 && overwrite == 0)
+	if (index >= 0 && overwrite == 0)
 		return ;
-	//if existed , and want to overwrite to it.
 	if (index >= 0 && overwrite == 1)
 	{
 		shell->my_envp[index] = gc_strdup(new_path, &gc->shell);
-		t_gc_list *find = find_node(gc->temp, (char*)new_path);
+		find = find_node(gc->temp, (char *)new_path);
 		delete_node(&gc->temp, find);
 	}
-	//even if is not existed then anyway we have to assign to it 
 	else if (index < 0)
 	{
 		new_envp = expand_envp(shell, new_path);
-		char **old_envp = shell->my_envp;
+		old_envp = shell->my_envp;
 		shell->my_envp = new_envp;
-		t_gc_list *old_envp_node = find_node(gc->shell, (char**)old_envp);
+		old_envp_node = find_node(gc->shell, (char **)old_envp);
 		delete_node(&gc->shell, old_envp_node);
 		shell->my_envp = new_envp;
 	}
 }
 
-void cd(char **args, t_shell *shell, t_gc *gc)
+void	cd(char **args, t_shell *shell, t_gc *gc)
 {
+	char		*target;
+	char		*new_dir;
+	t_gc_list	*find;
+
 	if (!shell)
 		return ;
-
 	shell->cur_dir = my_getcwd(gc);
 	if (!shell->cur_dir)
 	{
@@ -186,8 +170,6 @@ void cd(char **args, t_shell *shell, t_gc *gc)
 		gc_free(gc);
 		return ;
 	}
-	char	*target;
-	// char	*temp;
 	if (args[0] == NULL || strcmp(args[0], "~") == 0)
 	{
 		fprintf(stderr, RED"case just cd\n"DEFAULT);
@@ -222,64 +204,19 @@ void cd(char **args, t_shell *shell, t_gc *gc)
 		gc_free(gc);
 		exit(1);
 	}
-	char	*new_dir = my_getcwd(gc);
+	new_dir = my_getcwd(gc);
 	if (!new_dir)
 	{
 		perror(RED "chdir error for cd - \n"DEFAULT);
 		gc_free(gc);
 		exit(1);
 	}
-	t_gc_list *find = find_node(gc->temp, (char*)target);
+	find = find_node(gc->temp, (char *)target);
 	delete_node(&gc->temp, find);
 	shell->old_dir = shell->cur_dir;
 	ft_setenv("OLDPWD", shell->cur_dir, 1, shell);
 	fprintf(stderr, RED"sell->old_dir : %s\n"DEFAULT, shell->old_dir);
 	shell->cur_dir = new_dir;
 	ft_setenv("PWD", shell->cur_dir, 1, shell);
-	fprintf(stderr , RED"sell->cur_dir : %s\n"DEFAULT, shell->cur_dir);
+	fprintf(stderr, RED"sell->cur_dir : %s\n"DEFAULT, shell->cur_dir);
 }
-
-
-// int	main(int argc, char **argv, char **envp)
-// {
-// 	(void)argc;
-// 	(void)argv;
-// 	t_gc_list *head = init_gc_list();
-// 	t_shell *shell = init_shell_info();
-	
-// 	// int i = 0;
-// 	// while (test[i])
-// 	// {
-// 	// 	printf(BLUE "test[%d], %s:\n" DEFAULT ,i, test[i]);
-// 	// 	i++;
-// 	// }
-// 	//printf(YELLOW "head node %p\n",head);
-// 	//print_list(head);
-// 	//printf(BLUE "%p\n" DEFAULT, head->data);
-// 	//printf(BLUE "%p\n" DEFAULT, head->next->data); DEFAULT;
-	
-// 	shell->my_envp = copy_envp(head, envp);
-// 	//is_valid_dir("../../MINISHELL/EXECUTION");
-// 	//check_existing(shell_info->my_envp, "OLDPWD");
-
-// 	//test
-// 	int i = 0;
-// 	while (shell->my_envp[i])
-// 	{
-// 		printf(GREEN"%p,  %s\n"DEFAULT, shell->my_envp[i], shell->my_envp[i]);
-// 		i++;
-// 	}
-
-// 	ft_setenv("HOME", YELLOW"MY TEST?"DEFAULT, 1, shell);
-// 	ft_setenv(YELLOW "MYPWD" GREEN, YELLOW"??????????????????????"GREEN, 1, shell);
-	
-// 	// cd(argv, shell);
-
-// 	//test
-// 	i = 0;
-// 	while (shell->my_envp[i])
-// 	{
-// 		printf(BLUE"%p,  %s\n"DEFAULT, shell->my_envp[i], shell->my_envp[i]);
-// 		i++;
-// 	}
-// }
