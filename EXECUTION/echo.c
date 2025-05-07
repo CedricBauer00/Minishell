2dff131eabf748adac11fb1ac870f2b19c6dea3b
Jisoo Kim <jisokim2@3-C-2.42heilbronn.de>
2025-05-07 12:48:59 +0200

    git push

--- EXECUTION/echo.c
+++ EXECUTION/echo.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   echo.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cbauer < cbauer@student.42heilbronn.de>    +#+  +:+       +#+        */
+/*   By: jisokim2 <jisokim2@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/05 14:16:39 by jisokim2          #+#    #+#             */
-/*   Updated: 2025/05/07 09:44:51 by cbauer           ###   ########.fr       */
+/*   Updated: 2025/05/07 12:48:39 by jisokim2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,9 +20,15 @@ void	ft_echo(char **args, t_shell *shell)
 
 	gc = get_gc();
 	fprintf(stderr, RED"try to do 'echo' ft_echo() \n"DEFAULT);
-	if (ft_strncmp(args[0], "$", 1) == 0)
+	while (args[i] && strcmp(args[i], "-n") == 0)
 	{
-		if (args[1])
+		is_newline = false;
+		i++;
+	}
+	
+	while (args[i])
+	{
+		if (args[i][0] == '$' && ft_strlen(args[i] > 1))
 		{
 			char *var_name = *args + 1;
 			char *var = find_var_in_env(shell->my_envp, var_name, ft_strlen(var_name), gc->temp);
@@ -32,22 +38,14 @@ void	ft_echo(char **args, t_shell *shell)
 				// is_newline = true;
 				printf(YELLOW"\n"DEFAULT);
 			}
+			else
+			{
+				printf("%s", var);
+			}
 		}
-	}
-	else if (args[i])
-	{
-		while (args[i] && strcmp(args[i], "-n") == 0)
-		{
-			is_newline = false;
-			i++;
-		}
-		while (args[i] != NULL)
-		{
-			printf(YELLOW"%s", args[i]);
-			if (args[i + 1] != NULL)
-				printf(" ");
-			i++;
-		}
+		if (args[i + 1])
+			printf(" ");
+		i++;
 	}
 	if (is_newline)
 		printf(YELLOW"\n"DEFAULT);
