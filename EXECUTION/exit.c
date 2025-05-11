32d38b406ec571217ba47878ef045ce0b51d496d
Cedric Bauer <cbauer@3-C-1.42heilbronn.de>
2025-05-11 15:17:03 +0200

    fix exit function

--- EXECUTION/exit.c
+++ EXECUTION/exit.c
@@ -6,7 +6,7 @@
 /*   By: cbauer < cbauer@student.42heilbronn.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/10 13:53:04 by cbauer            #+#    #+#             */
-/*   Updated: 2025/05/11 14:29:54 by cbauer           ###   ########.fr       */
+/*   Updated: 2025/05/11 15:15:14 by cbauer           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -88,7 +88,7 @@ void	ft_exit(char **args, t_shell *shell)
 	i = 0;
 	while (args[0][i])
 	{
-		if (!(args[0][i] >= '0' && args[0][i] <= '9'))
+		if (!(args[0][i] >= '0' && args[0][i] <= '9') || (args[0][i] >= '0' && args[0][i] <= '9'))
 			printf("exit: %s numeric argument required\n", args[0]);
 		gc_free(gc);
 		exit(255);
@@ -104,3 +104,67 @@ void	ft_exit(char **args, t_shell *shell)
 	exit(shell->last_status_exit);
 	gc_free(gc);
 }
+
+// int	ft_atoll_helper(const char *str, int digit, long long num, int sign)
+// {
+// 	write(1, "here!\n", 6);
+// 	while (*str)
+// 	{
+// 		if (*str < '0' || *str > '9')
+// 			return (0);
+// 		digit = *str - '0';
+// 		if (num > (LONGLONGMAX / 10) || (num == (LONGLONGMAX / 10)
+// 				&& digit > LONGLONGMAX % 10))
+// 		{
+// 			printf("here!\n");
+// 			return (-1);
+// 		}
+// 		if (num < (LONGLONGMIN / 10) || (num == (LONGLONGMIN / 10)
+// 				&& digit > -(LONGLONGMIN % 10)))
+// 		{
+// 			printf("here2\n");
+// 			// *exitvalue = 255;
+// 			return (-1);
+// 		}
+// 		num = num * 10 + digit;
+// 		str++;
+// 	}
+// 	num *= sign;
+// 	if (num > LONGLONGMAX || num < LONGLONGMIN)
+// 		return (-1);
+// 	return (num);
+// }
+
+// long long	ft_atoll(const char *str, long long *exitvalue)
+// {
+// 	int			sign;
+// 	long long	num;
+// 	printf("here4!\n");
+
+// 	num = 0;
+// 	sign = 1;
+// 	if (!str)
+// 		return (0);
+// 	while (*str == ' ' || (*str >= 9 && *str <= 13))
+// 		str++;
+// 	if (*str == '-' || *str == '+')
+// 	{
+// 		if (*str == '-')
+// 			sign *= -1;
+// 		str++;
+// 	}
+// 	num = ft_atoll_helper(str, 0, num, sign);
+// 	if (num < 0)
+// 	{
+// 		printf("here3!\n");
+// 		*exitvalue = 255;
+// 		return (-1);
+// 	}
+// 	num *= sign;
+// 	if (num > LONGLONGMAX || num < LONGLONGMIN)
+// 	{
+// 		*exitvalue = 255;
+// 		return (-1);
+// 	}
+// 	return (num);
+// }
